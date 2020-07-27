343c71b0f277132e8c4ba2e1749983e3e6e2fe1c
Leonid Tsaplin <leonid.tsc@gmail.com>
2020-07-27 15:01:05 +0300

    First home work on android

--- app/src/main/java/com/tsc/leonid/android_geekbrains/MainActivity.java
+++ app/src/main/java/com/tsc/leonid/android_geekbrains/MainActivity.java
@@ -3,12 +3,38 @@ package com.tsc.leonid.android_geekbrains;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonGW;
+    private TextView textRes;
+    private EditText textLoc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+        setOnButtonGWClickBehaviour();
+    }
+
+    private void initViews(){
+        buttonGW = findViewById(R.id.button);
+        textRes = findViewById(R.id.textView);
+        textLoc = findViewById(R.id.editTextLocation);
+    }
+
+    private void setOnButtonGWClickBehaviour(){
+        buttonGW.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String res = getString(R.string.ResOfWeather);
+                textRes.setText(res);
+            }
+        });
     }
 }
